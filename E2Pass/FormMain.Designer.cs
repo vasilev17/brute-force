23ce90e44ee225a4b12399c03579ceb86fcaefac
Valentin Vasilev <VVVasilev@nemetschek.bg>
2023-10-10 19:15:44 +0300

    Brute Force finished project

---
+++ E2Pass/FormMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace E2Pass
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.radioButtonButton = new System.Windows.Forms.RadioButton();
+            this.radioButtonTextBox = new System.Windows.Forms.RadioButton();
+            this.buttonRun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(103, 19);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(43, 42);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseDown);
+            // 
+            // radioButtonButton
+            // 
+            this.radioButtonButton.AutoSize = true;
+            this.radioButtonButton.ForeColor = System.Drawing.Color.Red;
+            this.radioButtonButton.Location = new System.Drawing.Point(12, 37);
+            this.radioButtonButton.Name = "radioButtonButton";
+            this.radioButtonButton.Size = new System.Drawing.Size(56, 17);
+            this.radioButtonButton.TabIndex = 1;
+            this.radioButtonButton.Text = "Button";
+            this.radioButtonButton.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTextBox
+            // 
+            this.radioButtonTextBox.AutoSize = true;
+            this.radioButtonTextBox.Checked = true;
+            this.radioButtonTextBox.ForeColor = System.Drawing.Color.Red;
+            this.radioButtonTextBox.Location = new System.Drawing.Point(12, 12);
+            this.radioButtonTextBox.Name = "radioButtonTextBox";
+            this.radioButtonTextBox.Size = new System.Drawing.Size(64, 17);
+            this.radioButtonTextBox.TabIndex = 2;
+            this.radioButtonTextBox.TabStop = true;
+            this.radioButtonTextBox.Text = "TextBox";
+            this.radioButtonTextBox.UseVisualStyleBackColor = true;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(185, 12);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 42);
+            this.buttonRun.TabIndex = 3;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Visible = false;
+            this.buttonRun.Click += new System.EventHandler(this.ButtonRun_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 73);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.radioButtonTextBox);
+            this.Controls.Add(this.radioButtonButton);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormCaptue";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.RadioButton radioButtonButton;
+        private System.Windows.Forms.RadioButton radioButtonTextBox;
+        private System.Windows.Forms.Button buttonRun;
+    }
+}
+
---
+++ E2Pass/FormMain.cs
@@ -0,0 +1,182 @@
+﻿using E2Pass.Resources;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace E2Pass
+{
+    public partial class FormMain : Form
+    {
+        #region CaptureFields
+
+        private bool _capturing;
+        private IntPtr _hCapturedWindow;
+        private IntPtr _hTextBoxWindow;
+        private IntPtr _hButtonWindow;
+
+        private Image _finderHome;
+        private Image _finderGone;
+        private Cursor _cursorDefault;
+        private Cursor _cursorFinder;
+
+        #endregion
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            #region CaptureInit
+
+            _cursorDefault = Cursor.Current;
+
+            using (var cursorStream = new MemoryStream(EmbeddedResources.Finder))
+                _cursorFinder = new Cursor(cursorStream);
+
+            _finderHome = EmbeddedResources.FinderHome;
+            _finderGone = EmbeddedResources.FinderGone;
+
+            pictureBox.Image = _finderHome;
+
+            #endregion
+        }
+
+        #region Capture
+
+        private void CaptureMouse(bool captured)
+        {
+            if (captured)
+            {
+                Win32.SetCapture(Handle);
+
+                Cursor.Current = _cursorFinder;
+                pictureBox.Image = _finderGone;
+            }
+            else
+            {
+                Win32.ReleaseCapture();
+
+                Cursor.Current = _cursorDefault;
+                pictureBox.Image = _finderHome;
+
+                if (_hCapturedWindow != IntPtr.Zero)
+                {
+                    if (radioButtonTextBox.Checked)
+                    {
+                        _hTextBoxWindow = _hCapturedWindow;
+                        radioButtonTextBox.ForeColor = Color.Green;
+                        radioButtonTextBox.Visible = false;
+                        radioButtonButton.Checked = true;
+                    }
+                    else if (radioButtonButton.Checked)
+                    {
+                        _hButtonWindow = _hCapturedWindow;
+                        radioButtonButton.ForeColor = Color.Green;
+                        radioButtonButton.Visible = false;
+                        radioButtonTextBox.Checked = true;
+                    }
+
+                    if (_hTextBoxWindow != IntPtr.Zero &&
+                        _hButtonWindow != IntPtr.Zero)
+                    {
+                        pictureBox.Visible = false;
+                        buttonRun.Visible = true;
+                    }
+
+                    WindowHighlighter.Refresh(_hCapturedWindow);
+                    _hCapturedWindow = IntPtr.Zero;
+                }
+            }
+
+            _capturing = captured;
+        }
+
+        private void HandleMouseMovements()
+        {
+            if (!_capturing)
+                return;
+
+            IntPtr hWnd = Win32.WindowFromPoint(
+                Cursor.Position);
+
+            if (_hCapturedWindow != IntPtr.Zero &&
+                _hCapturedWindow != hWnd)
+                WindowHighlighter.Refresh(_hCapturedWindow);
+
+            if (hWnd != IntPtr.Zero)
+            {
+                _hCapturedWindow = hWnd;
+
+                Win32.Rect rc = new Win32.Rect();
+                Win32.GetWindowRect(hWnd, ref rc);
+
+                WindowHighlighter.Highlight(hWnd);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case (int)Win32.WindowMessages.WM_LBUTTONUP:
+                    CaptureMouse(false);
+                    break;
+
+                case (int)Win32.WindowMessages.WM_RBUTTONDOWN:
+                    CaptureMouse(false);
+                    break;
+
+                case (int)Win32.WindowMessages.WM_MOUSEMOVE:
+                    HandleMouseMovements();
+                    break;
+            };
+
+            base.WndProc(ref m);
+        }
+
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                CaptureMouse(true);
+        }
+
+        private void SendTextAndClick(string text)
+        {
+            const int WM_SETTEXT = 0x000C;
+            const int BM_CLICK = 0x00F5;
+
+
+            Win32.SendMessage(
+                _hTextBoxWindow,
+                WM_SETTEXT,
+                IntPtr.Zero,
+                text);
+
+            Win32.SendMessage(
+                _hButtonWindow,
+                BM_CLICK,
+                IntPtr.Zero,
+                IntPtr.Zero);
+
+        }
+
+        #endregion
+
+        private void ButtonRun_Click(object sender, EventArgs e)
+        {
+
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var q = alphabet.Select(x => x.ToString());
+            int size = 3;
+
+            for (int i = 0; i < size - 1; i++)
+                q = q.SelectMany(x => alphabet, (x, y) => x + y);
+
+            foreach (var item in q)
+                SendTextAndClick(item);
+
+        }
+    }
+}
